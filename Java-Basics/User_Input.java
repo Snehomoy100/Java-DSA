f1784e8e690bdac62329c079c53b4582c4d72389
snehomoy100 <snehomoy100@gmail.com>
2021-05-28 23:34:47 +0530

    today's all example files are added & updated

--- Java-Basics/User_Input.java
+++ Java-Basics/User_Input.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class user_input {
+public class User_Input {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a =  sc.nextInt();
