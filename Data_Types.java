8df31589aae7df61e8d03fac20f4fbb0d8b16a54
snehomoy100 <snehomoy100@gmail.com>
2021-05-28 23:40:20 +0530

    today's all example files are added & updated

--- Data_Types.java
+++ Data_Types.java
@@ -1,8 +1,8 @@
 public class Data_Types {
     public static void main(String[] args) {
-        int a = 100; // to store integer values & range is from 0 to 2^32.
+        int a = 100; // to store integer values & range is from -2^32 to 2^32. It takes 4 bytes (32 bits) of memory.
 
-        long b = 575626359;
+        long b = 575626359; // to store high valued integer values & the range is from 2
 
         short c = 12;
 
