4078ce6824522d4973b48e2e2a7b7287c3a4c71b
snehomoy100 <snehomoy100@gmail.com>
2021-05-28 23:59:24 +0530

    today's all example files are added & updated

--- Data_Types.java
+++ Data_Types.java
@@ -1,11 +1,18 @@
 public class Data_Types {
     public static void main(String[] args) {
 
+        // All the primitive data types of Java.
+
+        
+        // What is a Primitive Data Type in Java ? 
+        // Ans: A primitive data type specifies the size and type of variable values, and it has no additional methods.
+
+
         int a = 100; // to store integer values & range is from -2^32 to 2^32. It takes 4 bytes (32 bits) of memory space.
 
         long b = 575626359; // to store high valued integer values & the range is from -2^64 to 2^64. It takes 8 bytes (64 bits) of memory space.
 
-        short c = 12; // to store characters (e.g - 'a', 'b') & range is from 0 to 65,535 (256 ASCII + Remainings are UNICODE). It takes 2 bytes (16 bits) of memory space.
+        short c = 12; // to store whole numbers from -2^16 to 2^16 & it takes 2 bytes of memory space.
 
         byte i = 122; // to store small integers & range is from -128 to 127. It takes 1 byte of memory space.
 
@@ -13,7 +20,7 @@ public class Data_Types {
 
         double f = 789.52; // to store fractionl numbers & the range is from 15 decimal digits. It takes 8 bytes of memory space.
 
-        char g = 'A';
+        char g = 'A'; // to store characters (e.g - 'a', 'b') & range is from 0 to 65,535 (256 ASCII + Remainings are UNICODE). It takes 2 bytes (16 bits) of memory space
 
         boolean h = true; // to store true or false values & it takes 1 byte of memory space.
     }
