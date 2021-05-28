16c5a5623b998b03716e02b07057f7f30661d711
snehomoy100 <snehomoy100@gmail.com>
2021-05-28 23:55:00 +0530

    today's all example files are added & updated

--- Data_Types.java
+++ Data_Types.java
@@ -1,5 +1,6 @@
 public class Data_Types {
     public static void main(String[] args) {
+
         int a = 100; // to store integer values & range is from -2^32 to 2^32. It takes 4 bytes (32 bits) of memory space.
 
         long b = 575626359; // to store high valued integer values & the range is from -2^64 to 2^64. It takes 8 bytes (64 bits) of memory space.
@@ -7,13 +8,13 @@ public class Data_Types {
         short c = 12; // to store characters (e.g - 'a', 'b') & range is from 0 to 65,535 (256 ASCII + Remainings are UNICODE). It takes 2 bytes (16 bits) of memory space.
 
         byte i = 122; // to store small integers & range is from -128 to 127. It takes 1 byte of memory space.
-        
-        float d = 9.8f; // to store decimal numbers & range is from 
 
-        double f = 789.52;
+        float d = 9.8f; // to store fractional numbers & range is from 6 to 7 decimal digits. It takes 4 bytes of memory space.
+
+        double f = 789.52; // to store fractionl numbers & the range is from 15 decimal digits. It takes 8 bytes of memory space.
 
         char g = 'A';
 
-        boolean h = true;
+        boolean h = true; // to store true or false values & it takes 1 byte of memory space.
     }
 }
