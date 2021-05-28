1176b13f0c6e86485e721b500374c3d5af84cb6d
snehomoy100 <snehomoy100@gmail.com>
2021-05-28 23:47:21 +0530

    today's all example files are added & updated

--- Data_Types.java
+++ Data_Types.java
@@ -6,7 +6,9 @@ public class Data_Types {
 
         short c = 12; // to store characters (e.g - 'a', 'b') & range is from 0 to 65,535 (256 ASCII + Remainings are UNICODE). It takes 2 bytes (16 bits) of memory space.
 
-        float d = 9.8f;
+        byte i = 122; // to store small integers & range is from -128 to 127. It takes 1 byte of memory space.
+        
+        float d = 9.8f; // to store decimal numbers & range is from 
 
         double f = 789.52;
 
