bc16699754059c94794f3b04db17d3d6d8281544
snehomoy100 <snehomoy100@gmail.com>
2021-05-28 23:38:27 +0530

    today's all example files are added & updated

---
+++ Data_Types.java
@@ -0,0 +1,17 @@
+public class Data_Types {
+    public static void main(String[] args) {
+        int a = 100; // to store integer values & range is from 0 to 2^32.
+
+        long b = 575626359;
+
+        short c = 12;
+
+        float d = 9.8f;
+
+        double f = 789.52;
+
+        char g = 'A';
+
+        boolean h = true;
+    }
+}
