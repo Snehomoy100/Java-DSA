5bccd4077955cfae77e6bf0a5192441e5529f1f6
snehomoy100 <snehomoy100@gmail.com>
2021-05-30 16:43:12 +0530

    today's all example files are added & updated

--- Java-Basics/Buffered_Reader.java
+++ Java-Basics/Buffered_Reader.java
@@ -13,24 +13,4 @@ class Test {
 }
 
 
-class Test_2{
-    public static void main(String[] args) {
-        int x = 10; 
-        int y = 20;
-        double z = Math.PI;
-
-        // normal outputs in Java.
-        System.out.print(x);
-        System.out.println(z);
-
-
-        // used for floating point numbers & formatting them with ease.
-        System.out.printf("The value of x is: %d\n", x);
-        System.out.printf("The value of y is: %d\n", y);
-        System.out.format("Value of PI is: %.2f\n", z);
-        System.out.format("Value of PI is: %5.3f\n", z);
-        System.out.format("Value of PI is: %08.4f\n", z);
-
-    }
-}
 
