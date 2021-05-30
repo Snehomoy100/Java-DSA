28325187c6bd6eb201ce2cd01dc624c01c63cbef
snehomoy100 <snehomoy100@gmail.com>
2021-05-30 16:42:07 +0530

    today's all example files are added & updated

---
+++ Java-Basics/Java_Output.java
@@ -0,0 +1,20 @@
+public class Java_Output {
+    public static void main(String[] args) {
+        int x = 10; 
+        int y = 20;
+        double z = Math.PI;
+
+        // normal outputs in Java.
+        System.out.print(x);
+        System.out.println(z);
+
+
+        // used for floating point numbers & formatting them with ease.
+        System.out.printf("The value of x is: %d\n", x);
+        System.out.printf("The value of y is: %d\n", y);
+        System.out.format("Value of PI is: %.2f\n", z);
+        System.out.format("Value of PI is: %5.3f\n", z);
+        System.out.format("Value of PI is: %08.4f\n", z);
+
+    }
+}
