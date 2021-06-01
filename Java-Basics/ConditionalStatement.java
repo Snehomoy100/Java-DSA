25645703a341ac8861ce01babd2ab0aa78f5c2ae
snehomoy100 <snehomoy100@gmail.com>
2021-06-01 20:51:40 +0530

    today's all example files are added & updated

--- Java-Basics/ConditionalStatement.java
+++ Java-Basics/ConditionalStatement.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 public class ConditionalStatement {
     public static void main(String[] args) {
-        if (condition) {
-            //
-        } else if(){
-            //
+
+        // Question: Given a number n, find if the number is even or odd
+        System.out.println("Enter a number: ");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        if (n % 2 == 0) {
+            System.out.println(n + "is an even no.");
         } else {
-            //
+            System.out.println(n + "is an odd no.");
         }
+        input.close();
     }
 }
