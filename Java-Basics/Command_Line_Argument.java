68ead6970ad2eb97a75d978c397f5053f294e13c
snehomoy100 <snehomoy100@gmail.com>
2021-06-01 19:01:32 +0530

    today's all example files are added & updated

---
+++ Java-Basics/Command_Line_Argument.java
@@ -0,0 +1,9 @@
+public class Command_Line_Argument {
+    public static void main(String[] args){
+        if(args.length > 0){
+            System.out.println("Arguments are there.");
+        } else {
+            System.out.println("No arguments are there");
+        }
+    }
+}
