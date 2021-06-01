ffc05391d83dc4edc7192ea8b506275471a30546
snehomoy100 <snehomoy100@gmail.com>
2021-06-01 19:20:51 +0530

    today's all example files are added & updated

--- Java-Basics/Operators.java
+++ Java-Basics/Operators.java
@@ -1,5 +1,20 @@
 public class Operators {
     public static void main(String[] args){
         // operators
+    
+
+        // Artithmetic operators
+        int a = 10;
+        int b = 12;
+        int c = 12;
+        int z = (a + b * c); // precidence 
+        System.out.println(z);
+
+        
+        // Unary operators
+        z = a++; // post increment & similar is the post decrement as well. e.g - z = a--;
+        System.out.println(z + ' ' + a);
+        z = ++a; // pre increment & similar is the pre decrement as well. e.g - z = --a;
+        System.out.println(z + ' ' + a);
     }
 }
