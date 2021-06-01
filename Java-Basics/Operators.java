e7ef43d75881a660dbd3849ceb04c8b63f683270
snehomoy100 <snehomoy100@gmail.com>
2021-06-01 20:42:06 +0530

    today's all example files are added & updated

--- Java-Basics/Operators.java
+++ Java-Basics/Operators.java
@@ -71,3 +71,16 @@ class ShortCircuit {
         }
     }
 }
+
+class RelationalOperator {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x >= y);
+        System.out.println(x <= y);
+        System.out.println(x > y);
+        System.out.println(x < y);
+    }
+}
