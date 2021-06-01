1b9babad9f94d6715bdd61c0ae00d140a269e21d
snehomoy100 <snehomoy100@gmail.com>
2021-06-01 19:33:34 +0530

    today's all example files are added & updated

--- Java-Basics/Operators.java
+++ Java-Basics/Operators.java
@@ -10,11 +10,22 @@ public class Operators {
         int z = (a + b * c); // precidence 
         System.out.println(z);
 
-        
+
         // Unary operators
         z = a++; // post increment & similar is the post decrement as well. e.g - z = a--;
-        System.out.println(z + ' ' + a);
+        System.out.println(z + " " + a);
         z = ++a; // pre increment & similar is the pre decrement as well. e.g - z = --a;
-        System.out.println(z + ' ' + a);
+        System.out.println(z + " " + a);
+
+
+        // Assignment operators
+        int x = 10;
+        int y = 5;
+        x += y; // => x = x + y
+        System.out.println(x);
+        x %= y; // x = x % y (% gives the remainder after deviding x with y)
+        System.out.println(x);
+        int s = x = y; // here comes the associativity with is Right to Left for assignment operators.
+        System.out.println(s);
     }
 }
