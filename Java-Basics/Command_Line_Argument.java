fce015575935229fda1f171c56cc3e9538620589
snehomoy100 <snehomoy100@gmail.com>
2021-06-01 19:03:17 +0530

    today's all example files are added & updated

--- Java-Basics/Command_Line_Argument.java
+++ Java-Basics/Command_Line_Argument.java
@@ -1,7 +1,7 @@
 public class Command_Line_Argument {
     public static void main(String[] args){
         if(args.length > 0){
-            System.out.println("Arguments are there.");
+            System.out.println("Arguments are there."); 
         } else {
             System.out.println("No arguments are there");
         }
---
+++ Java-Basics/Operators.java
@@ -0,0 +1,5 @@
+public class Operators {
+    public static void main(String[] args){
+        // operators
+    }
+}
