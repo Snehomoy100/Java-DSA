6e19e54460fdfc68ea9d79a5cf76bd2188d2e65d
snehomoy100 <snehomoy100@gmail.com>
2021-06-07 23:28:37 +0530

    today's all example files are added & updated

--- Java-Basics/ForLoop.java
+++ Java-Basics/ForLoop.java
@@ -1,7 +1,15 @@
-public class ForLoop {
+class ForLoop {
     public static void main(String[] args){
         for(int i = 0; i<10; i++){ // example of for loop
             System.out.println("Snehomoy");
+        }
+    }
+}
+
+class InfiniteLoop {
+    public static void main(String[] args) {
+        for(;true;){ // example of infinite loop
+            System.out.println("Maitra");
         }
     }
 }
