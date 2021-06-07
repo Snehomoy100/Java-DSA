cf733f28b45f0925bf0c05aef7a9aaa89b9e7232
snehomoy100 <snehomoy100@gmail.com>
2021-06-07 23:18:56 +0530

    today's all example files are added & updated

--- Java-Basics/ForLoop.java
+++ Java-Basics/ForLoop.java
@@ -1,6 +1,6 @@
 public class ForLoop {
     public static void main(String[] args){
-        for(int i = 0; i<10; i++){
+        for(int i = 0; i<10; i++){ // example of for loop
             System.out.println("Snehomoy");
         }
     }
