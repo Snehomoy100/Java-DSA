cc1fc2a300fe13415cb987cc600fd78dcf29c8d4
snehomoy100 <snehomoy100@gmail.com>
2021-06-07 23:49:55 +0530

    today's all example files are added & updated

--- Java-Basics/Loop.java
+++ Java-Basics/Loop.java
@@ -1,10 +1,19 @@
-class ForLoop {
+class Loop {
     public static void main(String[] args){
         for(int i = 0; i<10; i++){ // example of for loop
             System.out.println("Snehomoy");
         }
+
+
+        int i = 0;
+        while (i < 10) { // example of while loop
+            System.out.println("Snehomoy");
+            i = i + 2;
+        }
     }
 }
+
+
 
 class InfiniteLoop {
     public static void main(String[] args) {
