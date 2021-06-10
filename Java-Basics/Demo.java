426f056a7c33c49d06dbb395968f7f4b538fe067
snehomoy100 <snehomoy100@gmail.com>
2021-06-11 01:50:51 +0530

    today's all example files are added & updated

---
+++ Java-Basics/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void demo(int a, int b){
+		System.out.println(a + " " + b);
+    }
+
+    public static void main(String[] args) {
+		int a = 5;
+		int b = 15; 
+		demo(a);
+    }
+}
