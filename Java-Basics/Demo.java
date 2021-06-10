854add9cab9cd717aa5a868a04aa5612b4a406fc
snehomoy100 <snehomoy100@gmail.com>
2021-06-11 01:59:56 +0530

    today's all example files are added & updated

--- Java-Basics/Demo.java
+++ Java-Basics/Demo.java
@@ -1,4 +1,5 @@
-public class Demo {
+
+class Demo {
     public static void demo(int a, int b){
 		System.out.println(a + " " + b);
     }
@@ -7,5 +8,32 @@ public class Demo {
 		int a = 5;
 		int b = 15; 
 		demo(a);
+    }
+}
+
+class PrimeTwoToN {
+
+    public static boolean isPrime(int n){
+        int d = 2;
+        for(int i = 1; i <= n; i++){
+            if (n % d == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void printTwoToN(int n){
+        for(int i = 2; i <= n; i++){
+            boolean iIsPrime = isPrime(i); // function calling inside on another function
+            if (iIsPrime) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printTwoToN(100);
     }
 }
