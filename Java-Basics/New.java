8b7c815181aeadbfd1b63616f4170c552c129a93
snehomoy100 <snehomoy100@gmail.com>
2021-06-13 19:58:41 +0530

    today's all example files are added & updated

---
+++ Java-Basics/New.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class New {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println(n + 1);
+        // System.out.println("just a demo");
+        input.close();
+    }
+}
