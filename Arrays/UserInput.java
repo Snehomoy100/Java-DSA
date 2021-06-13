b540f4222a32dac97388aa523b6764f83c7346c9
snehomoy100 <snehomoy100@gmail.com>
2021-06-14 00:18:32 +0530

    today's all example files are added & updated

---
+++ Arrays/UserInput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class UserInput{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int arr[] = new int[n];
+
+        for(int i = 0; i<n; i++){
+            System.out.println("Enter the element at " + i + "th position: ");
+            arr[i] = input.nextInt();
+        }
+
+        System.out.println("Your array is: ");
+
+        for(int j=0; j<n; j++){
+            System.out.println(arr[j]);
+        }
+
+        input.close();
+    }
+}
