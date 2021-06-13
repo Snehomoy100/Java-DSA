e4f31270a86ef5d7240aa8cc0d5a298665d0bfab
snehomoy100 <snehomoy100@gmail.com>
2021-06-13 23:52:59 +0530

    today's all example files are added & updated

--- Arrays/Array.java
+++ Arrays/Array.java
@@ -3,5 +3,11 @@ public class Array {
         int arr[] = new int[6];
         arr[5] = 10;
         System.out.println(arr[5]);
+
+        char cArray[] = new char[3]; // Arrays assign default value after declaration
+        System.out.println(cArray[1]);
+
+        double dArray[] = new double[5];
+        System.out.println(dArray[4]); 
     }
 }
