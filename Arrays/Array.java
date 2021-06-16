f4e39a356487155f9c9a283f8726b65b5a240eab
snehomoy100 <snehomoy100@gmail.com>
2021-06-17 00:59:57 +0530

    today's all example files are added & updated

--- Arrays/Array.java
+++ Arrays/Array.java
@@ -1,5 +1,5 @@
 public class Array {
-    public static void main(String[] args) {
+public static void main(String[] args) {
         int arr[] = new int[6];
         arr[5] = 10;
         System.out.println(arr[5]);
