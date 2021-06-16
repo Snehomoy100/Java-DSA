14ec0ff1ada5aac689e819984012af57b86b4abb
snehomoy100 <snehomoy100@gmail.com>
2021-06-17 03:56:40 +0530

    today's all example files are added & updated

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+public class LinearSearch {
+
+
+    public static int linearSearch(int arr[], int x){
+        for(int i = 0; i<arr.length; i++){
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int[] takeInput(){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int arr[] = new int[n];
+
+        for(int index = 0; index<n; index++){
+            System.out.println("Enter the element at " + index + "th position: ");
+            arr[index] = input.nextInt();
+        }
+
+        return arr;
+    }
+    public static void main(String[] args) {
+        int searchElement = 10;
+        int arr[] = takeInput();
+        int searchIndex = linearSearch(arr, searchElement);
+        System.out.println(searchIndex);
+    }
+}
