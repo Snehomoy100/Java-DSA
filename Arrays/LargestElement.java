a463cd1563d9cf7763525da1ce57ec89747851b3
snehomoy100 <snehomoy100@gmail.com>
2021-06-17 03:18:34 +0530

    today's all example files are added & updated

---
+++ Arrays/LargestElement.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class LargestElement {
+
+    public static int largestInArrray(int arr[]){
+        int max = Integer.MIN_VALUE;
+        for(int index=0; index<arr.length; index++){
+            if (max < arr[index]) {
+                max = arr[index];
+            } 
+        }
+
+        return max;
+    }
+
+    public static int[] takeInput(){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println("No. of elements in the array: ");
+        int arr[] = new int[n];
+
+        for(int index = 0; index<n; index++){
+            System.out.println("Enter the element at " + index + "th position: ");
+            arr[index] = input.nextInt();
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = takeInput();
+        int largestOfArray = largestInArrray(arr);
+        System.out.println("Largest element in the array is: " + largestOfArray);
+    }
+}
