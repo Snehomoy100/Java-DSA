506b2a0fc3e4b6e10fa8f2dcfdfe7710b3a12273
snehomoy100 <snehomoy100@gmail.com>
2021-06-19 19:50:30 +0530

    today's all example files are added & updated

--- Arrays/ArrangeNumbers.java
+++ Arrays/ArrangeNumbers.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+
 
 public class ArrangeNumbers {
 
@@ -19,11 +19,4 @@ public class ArrangeNumbers {
             }
         }
     }
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int arr[] = new int[n];
-        arrangeNumbers(arr, 7);
-    }
-    
 }
