dcf12bdb73f822121a854ed0b54314ee26ac564d
snehomoy100 <snehomoy100@gmail.com>
2021-06-19 23:58:05 +0530

    today's all example files are added & updated

--- Arrays/Icrement.java
+++ Arrays/Icrement.java
@@ -3,16 +3,21 @@ public class Icrement {
 
     public static void arrayIncrement(int arr[]) {
 
-        arr[0] = 15; // this arr & the arr from main func is referring to the same reference
+        System.out.println(arr); // this arr & the arr from main func is referring to the same reference
         for(int index = 0; index < arr.length; index++){
             arr[index] += index;
         }
     }
 
-    
+
     public static void main(String[] args) {
+        
         int arr[] = {1, 2, 3, 4, 5};
+
         arrayIncrement(arr);
+
+        System.out.println(arr); // this arr & the arr from main func is referring to the same reference
+
         for(int i=0; i<arr.length; i++){
             System.out.println(arr[i]);
         }
