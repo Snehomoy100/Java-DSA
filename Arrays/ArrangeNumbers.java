91607843a779ac5c67c87d5f581dc82e4dbc62a1
snehomoy100 <snehomoy100@gmail.com>
2021-06-19 19:50:10 +0530

    today's all example files are added & updated

--- Arrays/ArrangeNumbers.java
+++ Arrays/ArrangeNumbers.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class ArrangeNumbers {
 
     public static void arrangeNumbers(int arr[], int sizeOfArray){
         int left = 0;
-        int right = n - 1;
+        int right = sizeOfArray - 1;
+        int value = 1;
+
+        while (left <= right) {
+            if (value % 2 == 1) {
+                arr[left] = value;
+                value++;
+                left++;
+            } else {
+                arr[right] = value;
+                value++;
+                right--;
+            }
+        }
     }
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int arr[] = new int[n];
+        arrangeNumbers(arr, 7);
     }
+    
 }
