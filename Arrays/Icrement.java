e1129cf3d93a4c2a1dd3b758805db0bd5bf5cc32
snehomoy100 <snehomoy100@gmail.com>
2021-06-20 00:17:48 +0530

    today's all example files are added & updated

--- Arrays/Icrement.java
+++ Arrays/Icrement.java
@@ -1,25 +1,33 @@
 public class Icrement {
     // Pass by value example in non-primitives
 
-    public static void arrayIncrement(int arr[]) {
+    public static int[] arrayIncrement(int arr[]) {
 
-        System.out.println(arr); // this arr & the arr from main func is referring to the same reference
+        // System.out.println(arr); // this arr & the arr from main func is referring to the same reference
+        arr = new int[8];
         for(int index = 0; index < arr.length; index++){
-            arr[index] += index;
+            arr[index] += 1;
+        }
+        return arr;
+    }
+
+
+
+    public static void printArray(int[] arr){
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
         }
     }
 
 
     public static void main(String[] args) {
-        
+
         int arr[] = {1, 2, 3, 4, 5};
 
-        arrayIncrement(arr);
+        arr = arrayIncrement(arr);
 
-        System.out.println(arr); // this arr & the arr from main func is referring to the same reference
+        // System.out.println(arr); // this arr & the arr from main func is referring to the same reference
 
-        for(int i=0; i<arr.length; i++){
-            System.out.println(arr[i]);
-        }
+        printArray(arr);
     }
 }
