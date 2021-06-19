4d7b0cd5b4f6b8a248704b4ff2bf3d887456e107
snehomoy100 <snehomoy100@gmail.com>
2021-06-20 01:27:56 +0530

    today's all example files are added & updated

---
+++ Arrays/AllPairs.java
@@ -0,0 +1,14 @@
+public class AllPairs {
+
+    public static void printAllPairs(int[] arr){
+        for(int i = 0; i<(arr.length-1); i++){
+            for(int j = i+1; j<arr.length; j++){
+                System.out.println("(" + arr[i] + ", " + arr[j] + ")");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 7, 8, 4, 2};
+        printAllPairs(arr);
+    }
+}
