81bd3da3d426533471baea49701f99f43a9cf4f4
snehomoy100 <snehomoy100@gmail.com>
2021-06-19 19:35:13 +0530

    today's all example files are added & updated

--- Arrays/ArrangeNumbers.java
+++ Arrays/ArrangeNumbers.java
@@ -1,5 +1,10 @@
 public class ArrangeNumbers {
+
+    public static void arrangeNumbers(int arr[], int sizeOfArray){
+        int left = 0;
+        int right = n - 1;
+    }
     public static void main(String[] args) {
-        
+
     }
 }
