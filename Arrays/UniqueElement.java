59f754f8b83a05676a4883b6507165950a9167b4
snehomoy100 <snehomoy100@gmail.com>
2021-06-23 22:55:12 +0530

    updated

--- Arrays/UniqueElement.java
+++ Arrays/UniqueElement.java
@@ -1,5 +1,9 @@
 public class UniqueElement {
-    public static void main(String[] args) {
+
+    public static int uniqueElement(){
         
+    }
+    public static void main(String[] args) {
+
     }
 }
