192d74b2f76fc2421a2134750fc94cde1c975890
snehomoy100 <snehomoy100@gmail.com>
2021-06-26 00:41:44 +0530

    updated

--- Arrays/UniqueElement.java
+++ Arrays/UniqueElement.java
@@ -1,9 +1,39 @@
+import java.util.Scanner;
+
 public class UniqueElement {
 
-    public static int uniqueElement(){
+    public static int uniqueElement(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            int j;
+            for(j=0; j<arr.length; j++){
+                if( i != j){
+                    if(arr[i] == arr[j]){
+                        break;
+                    }
+                }
+            }
+            if(j == arr.length){
+                return arr[i];
+            }
+        
+        }
+
+        return Integer.MAX_VALUE;
         
     }
-    public static void main(String[] args) {
 
+
+    public static int[] inputArray(int n){
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i < n; i++){
+            arr[i] = input.nextInt();
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = inputArray(5);
+        uniqueElement(arr);
     }
 }
