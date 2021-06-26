92b62a45c25ca5e59877d3f7b60902c8a8cf4cbd
snehomoy100 <snehomoy100@gmail.com>
2021-06-26 21:02:56 +0530

    updated

--- Arrays/UniqueElement.java
+++ Arrays/UniqueElement.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
-public class UniqueElement {
+class UniqueElementBruteForce {
+
+    // Time complexity for this approach is O(n^2)
 
     public static int uniqueElement(int[] arr){
+
         for(int i = 0; i < arr.length; i++){
             int j;
             for(j=0; j<arr.length; j++){
@@ -23,17 +26,57 @@ public class UniqueElement {
     }
 
 
-    public static int[] inputArray(int n){
+    public static int[] inputArray(){
         Scanner input = new Scanner(System.in);
-        n = input.nextInt();
+        int n = input.nextInt();
         int[] arr = new int[n];
         for(int i=0; i < n; i++){
             arr[i] = input.nextInt();
         }
         return arr;
     }
+
     public static void main(String[] args) {
-        int[] arr = inputArray(5);
-        uniqueElement(arr);
+        int[] arr = inputArray();
+        int uniqueNumber = uniqueElement(arr);
+        System.out.println();
+        System.out.println("The unique element in the array is: " + uniqueNumber);
+    }
+}
+
+
+
+
+
+
+class UniqueElementMostOptimal {
+
+    // Time complexity for this approach is O(n)
+
+    public static int uniqueElement(int[] arr){
+        int ans = 0;
+        for(int i=0; i<arr.length; i++){
+            ans = ans ^ arr[i];
+        }
+        return ans;
+    }
+
+
+    public static int[] takeInput(){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = input.nextInt();
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = takeInput();
+        int uniqueNumber = uniqueElement(arr);
+        System.out.println();
+        System.out.println(uniqueNumber);
     }
 }
