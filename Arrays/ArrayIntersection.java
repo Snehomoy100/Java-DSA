7bcce191d29994a4672b94c6b9e6eb47154e12bb
snehomoy100 <snehomoy100@gmail.com>
2021-07-08 20:49:52 +0530

    ArrayIntersection problem added

---
+++ Arrays/ArrayIntersection.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+
+class BruteForceSolution {
+
+
+
+
+
+    public static int[] takeInput(){
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = input.nextInt();
+        }
+
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr1 = takeInput();
+        int[] arr2 = takeInput();
+    }
+}
