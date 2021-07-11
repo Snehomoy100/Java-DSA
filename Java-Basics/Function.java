3fb6caf9c2ebbaaec64bd6da97a04270c6ee3f5b
snehomoy100 <snehomoy100@gmail.com>
2021-07-11 19:25:12 +0530

    Function.java file updated

--- Java-Basics/Function.java
+++ Java-Basics/Function.java
@@ -2,37 +2,38 @@ import java.util.Scanner;
 
 public class Function {
 
-    // function of factorial
+    // function for factorial
     public static int factorial(int n){
-        int ans = 1;
-        for(int i = 1; i <= n; i++){
-            ans = ans * i;
+        int fact = 1;
+        for(int i = 1; i<=n; i++){
+            fact = fact * i;
         }
-        return ans;
+
+        return fact;
     }
 
 
-    // function to check a no is prime or not
-    public static boolean isPrime(int n){
-        int d = 2;
-        while(d < n){
-            if (n % d == 0) {
-                return false;
-            }
-            d++;
-        }
-        return true;
-    }
+    // // function to check a no is prime or not
+    // public static boolean isPrime(int n){
+    //     int d = 2;
+    //     while(d < n){
+    //         if (n % d == 0) {
+    //             return false;
+    //         }
+    //         d++;
+    //     }
+    //     return true;
+    // }
 
-    // function to print all the numbers upto n
-    public static void noUpton(int n){ // void function returns nothing
-        if(n <= 0){
-            return; // return means termination of the function
-        }
-        for(int i = 1; i <= n; i++){
-            System.out.println(i);
-        }
-    }
+    // // function to print all the numbers upto n
+    // public static void noUpton(int n){ // void function returns nothing
+    //     if(n <= 0){
+    //         return; // return means termination of the function
+    //     }
+    //     for(int i = 1; i <= n; i++){
+    //         System.out.println(i);
+    //     }
+    // }
     
     // A program to find the NcR
     public static void main(String[] args) {
@@ -40,17 +41,17 @@ public class Function {
         int n = input.nextInt();
         int r = input.nextInt();
         
-        int numerator = factorial(n);
+        int numerator = factorial(n); // fucntion calling
 
         int denominator_one = factorial(r);
         
 
         int denominator_two = factorial(n - r);
 
-        boolean ansPrime = isPrime(19);
-        System.out.println(ansPrime);
+        // boolean ansPrime = isPrime(19);
+        // System.out.println(ansPrime);
         
-        noUpton(-8);
+        // noUpton(-8);
         // the formula of NcR is N!/(R! * (N - R)!)
         int NcR = numerator/(denominator_one * denominator_two);
         System.out.println(NcR);
