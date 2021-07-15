79c9eb7a7dc15743e20a364d915e1c482e4ed66c
snehomoy100 <snehomoy100@gmail.com>
2021-07-15 21:17:33 +0530

    InterSection.java file updated

--- Arrays/InterSection.java
+++ Arrays/InterSection.java
@@ -8,7 +8,7 @@ public class InterSection {
         for(int i=0; i<arr1.length; i++){
             for(int j=0; j<arr2.length; j++){
                 if(arr1[i] == arr2[j]){
-                     System.out.println("Array Intersection value is: " + arr2[j]);
+                     System.out.println(arr2[j] + " ");
                     arr2[j] = Integer.MIN_VALUE;
                     break;
                 }
