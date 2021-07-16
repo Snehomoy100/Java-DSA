f569b64d04f955d3b7719b66d0d0a36cc878c3d7
snehomoy100 <snehomoy100@gmail.com>
2021-07-16 23:38:33 +0530

    arrayIntersection function implemented in the ArrayIntersection.java file

--- Arrays/ArrayIntersection.java
+++ Arrays/ArrayIntersection.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
 
+public class ArrayIntersection {
 
-
-class BruteForceSolution {
-
-    
-
-    // TODO: Have to complete this section over here by implementing arrayIntersection function
+    public static void arrayIntersection(int[] arr1, int[] arr2){
+        for(int i=0; i<arr1.length; i++){
+            for(int j=0; j<arr2.length; j++){
+                if(arr1[i] == arr2[j]){
+                    System.out.println(arr2[j]);
+                    arr2[j] = Integer.MIN_VALUE;
+                    break;
+                }
+            }
+        }
+    }
 
 
 
     public static int[] takeInput(){
-
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         int[] arr = new int[n];
@@ -21,8 +26,14 @@ class BruteForceSolution {
 
         return arr;
     }
+
+
     public static void main(String[] args) {
+        System.out.println("Your first array: ");
         int[] arr1 = takeInput();
+        System.out.println("Your second array: ");
         int[] arr2 = takeInput();
+        System.out.println("Intersection part from the arrays are: \n");
+        arrayIntersection(arr1, arr2);
     }
 }
