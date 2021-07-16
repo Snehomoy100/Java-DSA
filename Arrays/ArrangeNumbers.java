5040273ff5afec9932e4ca6da1c5f242e54f8167
snehomoy100 <snehomoy100@gmail.com>
2021-07-16 22:07:53 +0530

    main function error resolved in the ArrangeNumbers.java file

--- Arrays/ArrangeNumbers.java
+++ Arrays/ArrangeNumbers.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 
 public class ArrangeNumbers {
 
-    public static void arrangeNumbers(int[] arr, int sizeOfArray){
+    public static void arrangeNumbers(int[] arr){
         int left = 0;
-        int right = sizeOfArray - 1;
+        int right = (arr.length - 1);
         int value = 1;
 
         while (left <= right) {
@@ -19,8 +20,33 @@ public class ArrangeNumbers {
         }
     }
 
-    public static void main(String[] args) {
 
+
+
+    public static int[] takeInput(){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] arr = new int[5];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = input.nextInt();
+        }
+
+        return arr;
+    }
+
+
+    public static void printArray(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = takeInput();
+        arrangeNumbers(arr);
+        System.out.println("\n");
+        System.out.println("The arranged array is: ");
+        printArray(arr);
     }
     
 }
