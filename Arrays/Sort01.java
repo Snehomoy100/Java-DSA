b8e21b0c923b180969ab47055b0f705e7d052a78
snehomoy100 <snehomoy100@gmail.com>
2021-07-16 18:43:31 +0530

    Sort01.java file updated

--- Arrays/Sort01.java
+++ Arrays/Sort01.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
+
+
 public class Sort01 {
     
+    
+    public static int[] takeInput(){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = input.nextInt();
+        }
+
+        return arr;
+    }     
+    
+    
+    public static void main(String[] args) {
+        
+    }
 }
