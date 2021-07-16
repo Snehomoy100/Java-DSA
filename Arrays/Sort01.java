00ae1075b2cdadb88c5764c3491e17c2a9488c62
snehomoy100 <snehomoy100@gmail.com>
2021-07-16 21:32:20 +0530

    Sort01.java file updated

--- Arrays/Sort01.java
+++ Arrays/Sort01.java
@@ -1,23 +1,28 @@
-import java.util.Scanner;
-
-
 
 public class Sort01 {
     
-    
-    public static int[] takeInput(){
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i] = input.nextInt();
+    public static void sortThe01(int[] arr){
+        int startingIndex = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == 0){
+                int temp = arr[startingIndex];
+                arr[startingIndex] = arr[i];
+                arr[i] = temp;
+                startingIndex += 1;
+            }
         }
-
-        return arr;
-    }     
+    } 
+    
+    public static void printArray(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
     
     
     public static void main(String[] args) {
-        
+        int[] arr = {0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1};
+        sortThe01(arr);
+        printArray(arr);
     }
 }
