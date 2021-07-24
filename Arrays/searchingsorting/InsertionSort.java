b0cbbbbf0380b4fe63b3c49961099064dd751cf2
snehomoy100 <snehomoy100@gmail.com>
2021-07-24 18:01:17 +0530

    InsertionSort.java file updated

---
+++ Arrays/searchingsorting/InsertionSort.java
@@ -0,0 +1,29 @@
+package searchingsorting;
+
+public class InsertionSort {
+    
+    public static void insertionSort(int[] arr){
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void printArray(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        int[] arr = {110, 32, 0, 76, 12, 8, 34, 12};
+        insertionSort(arr);
+        printArray(arr);
+    }
+}
