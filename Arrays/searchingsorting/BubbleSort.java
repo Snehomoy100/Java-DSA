b8df9bd06ccedb9ce052e6f0486eb7a31330030f
snehomoy100 <snehomoy100@gmail.com>
2021-07-24 17:56:12 +0530

    BubbleSort.java file updated

---
+++ Arrays/searchingsorting/BubbleSort.java
@@ -0,0 +1,31 @@
+package searchingsorting;
+
+public class BubbleSort {
+    
+    public static void bubbleSort(int[] arr){
+        for(int i=0; i<arr.length -1; i++){
+            for(int j=0; j<arr.length - i -1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+
+
+    public static void printArray(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    
+    public static void main(String[] args) {
+        int[] arr = {45, 0, 98, 12, 5, 4, 1};
+        bubbleSort(arr);
+        printArray(arr);
+    }
+}
