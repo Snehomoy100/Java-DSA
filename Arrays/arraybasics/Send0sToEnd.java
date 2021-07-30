1739d27f3900fa58d307524e9574dd1b013bec51
snehomoy100 <snehomoy100@gmail.com>
2021-07-31 00:12:16 +0530

    Send0sToEnd.java file added & updated

---
+++ Arrays/arraybasics/Send0sToEnd.java
@@ -0,0 +1,11 @@
+package arraybasics;
+
+public class Send0sToEnd {
+    
+    public static void send0sToEnd(int[] arr){
+        
+    }
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 0, 1, 0, 1, 1, 1};    
+    }
+}
