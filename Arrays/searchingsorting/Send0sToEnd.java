9a2e323eda216ad15d466ab5e4ecb553b50cc06b
snehomoy100 <snehomoy100@gmail.com>
2021-07-31 01:35:33 +0530

    Send0sToEnd.java file updated

--- Arrays/searchingsorting/Send0sToEnd.java
+++ Arrays/searchingsorting/Send0sToEnd.java
@@ -3,9 +3,35 @@ package searchingsorting;
 public class Send0sToEnd {
     
     public static void send0sToEnd(int[] arr){
+        if(arr.length==0 || arr.length==1)
+            return;
         
+        int right = 0;
+        int left = 0;
+        int temp;
+        while(right < arr.length){
+            if(arr[right] == 0){
+                right++;
+            } else {
+                temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                right++;
+                left++;
+            }
+        }
     }
+
+    public static void printArray(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+
     public static void main(String[] args) {
-        int[] arr = {0, 1, 0, 0, 1, 0, 1, 1, 1};    
+        int[] arr = {0, 1, 0, 0, 34, 0, 45, 5, 9};    
+        send0sToEnd(arr);
+        printArray(arr);
     }
 }
