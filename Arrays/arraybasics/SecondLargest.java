3aedef24e40db9397d3ad086347144696d837456
snehomoy100 <snehomoy100@gmail.com>
2021-08-03 00:50:37 +0530

    SecondLargest.java file updated

--- Arrays/arraybasics/SecondLargest.java
+++ Arrays/arraybasics/SecondLargest.java
@@ -1,11 +1,26 @@
 package arraybasics;
 
+import java.util.Scanner;
+
 public class SecondLargest{
 
-    
-    public static void main(String[] args) {
-        
+    public static int secondLargest(int[] arr){
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+    }
 
-        
+
+    public static int[] takeInput(){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = input.nextInt();
+        }
+
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = takeInput();
     }
 }
