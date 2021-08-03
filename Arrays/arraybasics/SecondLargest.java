d31bffc45c504071c9e02a15379d4410c77fa024
snehomoy100 <snehomoy100@gmail.com>
2021-08-04 01:15:26 +0530

    SecondLargest.java updated

--- Arrays/arraybasics/SecondLargest.java
+++ Arrays/arraybasics/SecondLargest.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class SecondLargest{
 
     public static int secondLargest(int[] arr){
-        if(arr.length < 2){
+
+        if(arr.length < 2){ //corner case if the size of array is 0 or 1
             return Integer.MIN_VALUE;
         }
 
@@ -14,9 +15,11 @@ public class SecondLargest{
         for(int i=0; i<arr.length; i++){
             if(arr[i] > max && arr[i] != max){
                 secondMax = max;
-                
+                max = arr[i];
             }
         }
+
+        return secondMax;
     }
 
 
@@ -32,5 +35,7 @@ public class SecondLargest{
     }
     public static void main(String[] args) {
         int[] arr = takeInput();
+        int secondLargestElement = secondLargest(arr);
+        System.out.println("The second largest element is: " + secondLargestElement);
     }
 }
