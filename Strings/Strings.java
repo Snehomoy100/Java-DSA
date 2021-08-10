e888bed0d7d7ac712f713057bea7527c87c57048
snehomoy100 <snehomoy100@gmail.com>
2021-08-11 00:54:17 +0530

    Strings.java file added & updated

--- Strings/Strings.java
+++ Strings/Strings.java
@@ -1,3 +1,16 @@
 public class Strings {
-    
+    public static void main(String[] args) {
+        char arr[] = {'C', 'o', 'd', 'i', 'n', 'g'};
+        System.out.println(arr[4]);
+
+
+        // Strings are char arrays under the hood
+        String str1 = "Snehomoy";
+        String str2 = ""; // empty string
+        System.out.println(str2.length());
+        String str3 = " "; // string with only one element
+        System.out.println(str3.length());
+        System.out.println(str1.charAt(3));
+        System.out.println(str1.length());
+    }
 }
