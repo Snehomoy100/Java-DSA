becffb000f7f27a6b5838da3491abd3906f6af91
snehomoy100 <snehomoy100@gmail.com>
2021-08-11 20:07:06 +0530

    PivotIndex.java file added & updated

--- Arrays/arraybasics/PivotIndex.java
+++ Arrays/arraybasics/PivotIndex.java
@@ -4,6 +4,9 @@ public class PivotIndex {
 
     public static int pivotIndex(int[] nums) {
         
+        // Time complexity is: O(n + n) = O(2n) ~= O(n)
+        // Space complexity is: O(1)
+
         // corner case for only one element in array
         if(nums.length == 1){
             return nums[0];
@@ -17,6 +20,7 @@ public class PivotIndex {
         
         // for more than two elements
         else{
+        // sum of the whole array
         int sum = 0;
         for(int i=0; i<nums.length; i++){
             sum += nums[i];
@@ -38,6 +42,8 @@ public class PivotIndex {
         
         return -1;
     }
+
+
     public static void main(String[] args) {
         int[] arr = {2, 3, 5, 1, 4};
         int pivotIndex = pivotIndex(arr);
