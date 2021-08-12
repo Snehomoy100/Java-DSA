a09c1e9750e4b04c97b5bbe26ce33df57a1dce29
snehomoy100 <snehomoy100@gmail.com>
2021-08-12 18:59:13 +0530

    SubString.java file added & updated

---
+++ Strings/SubString.java
@@ -0,0 +1,8 @@
+public class SubString {
+    public static void main(String[] args) {
+        String str = "Snehomoy";
+        System.out.println(str.substring(3));
+        System.out.println(str.substring(8)); // prints an empty string
+        System.out.println(str.substring(2, 6)); 
+    }
+}
