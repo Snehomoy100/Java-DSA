402441de4da1bf47b62264e6eb31755a8c7971ac
snehomoy100 <snehomoy100@gmail.com>
2021-08-15 19:19:52 +0530

    PalindromeOrNot.java file added & updated with the Most-Optimal solution

---
+++ Strings/PalindromeOrNot.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class PalindromeOrNot {
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+
+    public static boolean isPalindrome(String str){
+        // two-pointer approach
+        int left = 0;
+        int right = (str.length() - 1);
+
+        while(left < right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+
+            right--;
+            left++;
+        }
+
+        return true;
+    }
+
+
+    public static String takeInput(){
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+        return str;
+    }
+
+    public static void main(String[] args) {
+        String str = takeInput();
+        boolean palindromeOrNot = isPalindrome(str);
+        System.out.println(palindromeOrNot);
+    }
+}
