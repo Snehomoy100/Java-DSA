1afbc68fddaf765c2ecc8999fac9570e93c78adb
snehomoy100 <snehomoy100@gmail.com>
2021-08-16 17:22:07 +0530

    Strings.java file updated

---
+++ Strings/StringComparison.java
@@ -0,0 +1,15 @@
+public class StringComparison {
+
+    public static void stringComparison(String str, String str1){
+        if(str == str1){
+            System.out.println("Both are equal");
+        } else{
+            System.out.println("Both are not equal");
+        }
+    }
+    public static void main(String[] args) {
+        String str = "abc"; // stored inside of string pool
+        String str1 = new String("abc"); // stored inside of the Heap memory
+        stringComparison(str, str1);
+    }
+}
