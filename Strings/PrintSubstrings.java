249fc7951086cef8078dd5718c287f052cc4eb57
snehomoy100 <snehomoy100@gmail.com>
2021-08-19 19:38:12 +0530

    PrintSubstrings.java file updated with second approach

--- Strings/PrintSubstrings.java
+++ Strings/PrintSubstrings.java
@@ -1,5 +1,6 @@
 public class PrintSubstrings {
 
+    // First Approach
     public static void printAllSubstrings(String str){
         for(int startIndex=0; startIndex<str.length(); startIndex++){
             for(int endIndex = startIndex; endIndex<str.length(); endIndex++){
@@ -8,8 +9,18 @@ public class PrintSubstrings {
         }
     }
 
+    // Second Approach
+    public static void allSubstrings(String str){
+        for(int len=1; len<=str.length(); len++){
+            for(int start=0; start<=str.length()-len; start++){
+                int end = (start + len) - 1;
+                System.out.println(str.substring(start, end + 1));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String str = "abcd";
-        printAllSubstrings(str);
+        allSubstrings(str);
     }
 }
