ac5b2a45374d3122bdeb48c3cb8f175c94947a0e
snehomoy100 <snehomoy100@gmail.com>
2021-09-05 02:02:28 +0530

    UserInput.java file updated

--- Arrays/twodimensionalarrays/UserInput.java
+++ Arrays/twodimensionalarrays/UserInput.java
@@ -1,6 +1,12 @@
 package twodimensionalarrays;
 
 public class UserInput {
+
+    // How to take 2-D Array input in Java
+
+    public static int[][] takeInput(){
+        
+    }
     public static void main(String[] args) {
         
     }
