8646ff4d36f4c315c0cf0f06a03d5f8e899ddf14
snehomoy100 <snehomoy100@gmail.com>
2021-09-05 01:45:57 +0530

    TwoDimensionalArrays.java file updated

--- Arrays/twodimensionalarrays/TwoDimensionalArrays.java
+++ Arrays/twodimensionalarrays/TwoDimensionalArrays.java
@@ -2,6 +2,12 @@ package twodimensionalarrays;
 
 public class TwoDimensionalArrays {
     public static void main(String[] args) {
-        
+        int[][] arr = new int[4][5]; // [size of no of row][size or no of coloumn]
+        for(int i=0; i<4; i++){
+            for(int j=0; j<5; j++){
+                arr[i][j] = i * j;
+            }
+        }
+        System.out.println(arr[2][3]); // output: 6
     }
 }
