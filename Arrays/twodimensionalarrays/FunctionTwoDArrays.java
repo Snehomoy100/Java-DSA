997b48e5016d9bf3c99da39168c5cf59070e816b
snehomoy100 <snehomoy100@gmail.com>
2021-09-06 12:27:12 +0530

    FunctionTwoDArrays.java file added & updated

---
+++ Arrays/twodimensionalarrays/FunctionTwoDArrays.java
@@ -0,0 +1,9 @@
+package twodimensionalarrays;
+
+public class FunctionTwoDArrays {
+
+    // How to work with functions in 2-D arrays
+    public static void main(String[] args) {
+        
+    }
+}
