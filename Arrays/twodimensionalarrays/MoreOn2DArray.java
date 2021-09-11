cece0b382f8322d3bdd90e9450dad20197fd5aab
snehomoy100 <snehomoy100@gmail.com>
2021-09-11 23:56:05 +0530

    MoreOn2DArray.java file updated

--- Arrays/twodimensionalarrays/MoreOn2DArray.java
+++ Arrays/twodimensionalarrays/MoreOn2DArray.java
@@ -2,6 +2,29 @@ package twodimensionalarrays;
 
 public class MoreOn2DArray {
     public static void main(String[] args) {
-        
+        int[][] arr2d = new int[4][5];
+        System.out.println(arr2d); // prints the reference address of the arr2d
+
+        int[][] arr2dTwo = new int[3][];
+        for(int i=0; i<arr2dTwo.length; i++){
+            System.out.println(arr2dTwo[i]);
+        }
+        System.out.println(arr2dTwo[1][2]); // throws a NullPointerException
+
+
+        int[][] arr2dThree = new int[4][];
+        for(int i=0; i<arr2dThree.length; i++){
+            arr2dThree[i] = new int[i+2]; // initialisation of a 2-D array
+        }
+        for(int i=0; i<arr2dThree.length; i++){
+            for(int j=0; j<arr2dThree[i].length; j++){
+                System.out.print(arr2dThree[i][j] + " "); // prints the whole 2-D array
+            }
+            System.out.println();
+        }
+
+
+        int[][] another2DArray = new int[][4]; // 2-D array can't be created without the no. of rows (no. of rows is mandatory)
+        System.out.println(another2DArray[2][3]);
     }
 }
