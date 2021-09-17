c5a140177948b1c1e8e3882f6580717bfd028f23
snehomoy100 <snehomoy100@gmail.com>
2021-09-17 18:57:15 +0530

    Recursion.java file updated

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,31 +1,17 @@
 public class Recursion {
 
     // Why we need & use Recursion
-
-    public static void message(){
-        System.out.println("Snehomoy Maitra");
-        message1();
-    }
-
-    public static void message1(){
-        System.out.println("Snehomoy Maitra");
-        message2();
-    }
-
-    public static void message2(){
-        System.out.println("Snehomoy Maitra");
-        message3();
-    }
-
-    public static void message3(){
-        System.out.println("Snehomoy Maitra");
-        message4();
-    }
-
-    public static void message4(){
-        System.out.println("Snehomoy Maitra");
-    }
     public static void main(String[] args) {
-        message();
+        print(1);
+    }
+
+    public static void print(int n){
+        if(n == 5){ // base case or base condition
+            System.out.println(n);
+            return;
+        }
+
+        System.out.println(n);
+        print(n + 1);
     }
 }
