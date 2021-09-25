62cf73f7409aaf3354fa767c9007755f8a8b0d6d
snehomoy100 <snehomoy100@gmail.com>
2021-09-25 23:33:24 +0530

    BinarySearchRecursion.java is updated

--- Recursion/BInarySearchRecursion.java
+++ Recursion/BInarySearchRecursion.java
@@ -1,7 +1,24 @@
 public class BInarySearchRecursion {
 
-    public static int binarySearch(){
-        
+    public static int binarySearch(int[] arr, int x){
+        int n = arr.length;
+        int start = arr[0];
+        int end = arr[n - 1];
+        int mid = (start + end) / 2;
+
+        while(start <= end){
+            if(arr[mid] > x){
+                end = (mid - 1);
+            }
+            else if(arr[mid] == x){
+               return mid;
+            }
+            else{
+                start = (mid + 1);
+            }
+        }
+
+        return -1;
     }
     public static void main(String[] args) {
         
