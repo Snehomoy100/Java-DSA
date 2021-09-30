f9a60866e30d0d2e2c1e405b2fa61516603417ac
snehomoy100 <snehomoy100@gmail.com>
2021-09-30 21:53:59 +0530

    problemsonarray package created & ArrayIntersection.java file added & updated

--- Arrays/problemsonarray/ArrayIntersection.java
+++ Arrays/problemsonarray/ArrayIntersection.java
@@ -1,6 +1,10 @@
 package problemsonarray;
 
 public class ArrayIntersection {
+
+    public static void arrayIntersection(int[] arr1, int[] arr2){
+        
+    }
     public static void main(String[] args) {
         
     }
