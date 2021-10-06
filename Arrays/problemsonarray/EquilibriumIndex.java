1f17f5b7719c3f882d1afa66260de55f37d52d78
snehomoy100 <snehomoy100@gmail.com>
2021-10-06 21:05:23 +0530

    Equilibrium.java file updated with the most optimal solution

---
+++ Arrays/problemsonarray/EquilibriumIndex.java
@@ -0,0 +1,39 @@
+package problemsonarray;
+
+public class EquilibriumIndex {
+
+
+    public static int totalSum(int[] arr){
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+
+    public static int equilibriumIndex(int[] arr){
+        int leftSum = 0;
+        int rightSum = totalSum(arr);
+        int i = 0;
+        
+        while(i < arr.length){
+            rightSum = rightSum - arr[i];
+
+            if(leftSum == rightSum){
+                return i;
+            }
+            i = i + 1;
+
+            leftSum = leftSum - arr[i - 1];
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,7,3,6,5,6};
+        int theEquilibriumIndex = equilibriumIndex(arr);
+        System.out.println("The equilibrium index of the given array is: " + theEquilibriumIndex);
+    }
+}
