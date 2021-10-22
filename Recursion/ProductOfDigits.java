60cc257cc27bbeedd2672c3247831b8d7412939c
snehomoy100 <snehomoy100@gmail.com>
2021-10-22 22:40:50 +0530

    ProductOfDigits.java file added & updated

---
+++ Recursion/ProductOfDigits.java
@@ -0,0 +1,14 @@
+public class ProductOfDigits {
+
+    
+    public static int productOfDigits(int n){
+        if(n % 10 == n){ // base case
+            return n;
+        }
+
+        return ((n % 10) * productOfDigits(n / 10));
+    }
+    public static void main(String[] args) {
+        System.out.println(productOfDigits(3852));
+    }
+}
---
+++ Recursion/sample.java
@@ -0,0 +1,6 @@
+public class sample {
+    public static void main(String[] args) {
+        int n = 3 % 10;
+        System.out.println(n);
+    }
+}
