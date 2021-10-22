93a792bf88c808dc4aabbc7f2630e09c05fbb16a
snehomoy100 <snehomoy100@gmail.com>
2021-10-22 22:46:28 +0530

    ReverseNumber.java file added & updated

---
+++ Recursion/ReverseNumber.java
@@ -0,0 +1,11 @@
+public class ReverseNumber {
+    
+    // reverse a number using recursion
+    public static int reverseNumber(int n){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
