ee8363c68faec64fb309c753e07ad3871fb759eb
snehomoy100 <snehomoy100@gmail.com>
2021-10-23 12:59:34 +0530

    PowerOfNumber.java file added & updated

---
+++ Recursion/PowerOfNumber.java
@@ -0,0 +1,25 @@
+public class PowerOfNumber {
+
+    // calculate the power of a number recursively
+    public static int powerOfNumber(int x, int n){
+        
+        if(x == 0 && n == 0){
+            return 0;
+        }
+
+        if(x == 0){
+            return 0;
+        }
+
+        if(n == 0){
+            return 1;
+        }
+
+        return(x * (powerOfNumber(x, (n-1))));
+    }
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 6;
+        System.out.println(powerOfNumber(x, n));
+    }
+}
