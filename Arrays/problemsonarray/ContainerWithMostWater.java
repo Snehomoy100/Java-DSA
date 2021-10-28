1a9e9f26ed9f481200b2649e36d2edc7241a5d7d
snehomoy100 <snehomoy100@gmail.com>
2021-10-28 22:04:41 +0530

    ContainerWithMostWater.java file added & updated with the most optimal solution

--- Arrays/problemsonarray/ContainerWithMostWater.java
+++ Arrays/problemsonarray/ContainerWithMostWater.java
@@ -18,10 +18,37 @@ public class ContainerWithMostWater {
         }
         return maxArea;
     }
+
+    // Most Optimal Approach
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    
+    public static int containerWithMostWaterMostOptimal(int[] arr){
+        int i = 0;
+        int j = arr.length - 1;
+
+        int water  = 0;
+
+        while(i < j){
+            int height = Math.min(arr[i], arr[j]);
+            int width = (j - i);
+
+            water = Math.max(water, (height * width));
+
+            if(arr[i] < arr[j]){
+                i++;
+            } else{
+                j--;
+            }
+        }
+
+        return water;
+    }
     
     public static void main(String[] args) {
         int[] arr = {1,8,6,2,5,4,8,3,7};
-        int maximumArea = containerWithMostWater(arr);
+        // int maximumArea = containerWithMostWater(arr);
+        int maximumArea = containerWithMostWaterMostOptimal(arr);
         System.out.println(maximumArea);
     }
 }
