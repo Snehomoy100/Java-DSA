2c0615d6b63f447d2f64968da70312abd4dc822c
snehomoy100@gmail.com <snehomoy100@gmail.com>
2021-11-22 11:16:17 +0530

    Time & Space Complexity package added & updated

--- TimeSpaceComplexity/Complexity.java
+++ TimeSpaceComplexity/Complexity.java
@@ -1,5 +1,8 @@
 public class Complexity {
     public static void main(String[] args) {
-        
+        int i;
+        for(i = 0; i<5; ++i){
+            System.out.println(i);
+        }
     }
 }
