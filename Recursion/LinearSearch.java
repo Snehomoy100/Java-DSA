d2b257ee5b65fdff557d2f91fe7279728e3e18d0
snehomoy100@gmail.com <snehomoy100@gmail.com>
2021-11-29 19:07:02 +0530

    LinearSearch.java file added & updated with another solution

--- Recursion/LinearSearch.java
+++ Recursion/LinearSearch.java
@@ -1,7 +1,8 @@
 public class LinearSearch {
     public static void main(String[] args) {
         int[] arr = {12, 3, 56, 0, 6, 0};
-        System.out.println(linearSearch(arr, 0, 0));
+        System.out.println(linearSearch(arr, 0, 0)); // from 0th index
+        System.out.println(linearSearchFromLast(arr, arr.length, 56)); // from (n - 1)th index
     }
 
     public static int linearSearch(int[] arr, int index, int x){
@@ -14,6 +15,19 @@ public class LinearSearch {
             return index;
         } else{
             return(linearSearch(arr, index + 1, x));
+        }
+    }
+
+    public static int linearSearchFromLast(int[] arr, int index, int x){
+        // base case
+        if(index == -1){
+            return -1;
+        }
+
+        if(arr[index] == x){
+            return index;
+        } else{
+            return(linearSearchFromLast(arr, index - 1, x));
         }
     }
 }
