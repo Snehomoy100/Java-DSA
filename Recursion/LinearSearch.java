53329b86f596773e87bbefd2dea378ce69f58218
snehomoy100@gmail.com <snehomoy100@gmail.com>
2021-11-29 20:58:53 +0530

    LinearSearch.java file added & updated with multiple occurances solution using ArrayList

--- Recursion/LinearSearch.java
+++ Recursion/LinearSearch.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
 
+
 public class LinearSearch {
     public static void main(String[] args) {
         int[] arr = {12, 3, 56, 0, 6, 0};
         System.out.println(linearSearch(arr, 0, 0)); // from 0th index
-        System.out.println(linearSearchFromLast(arr, arr.length, 56)); // from (n - 1)th index
-        linearSearchMultipleOccurances(arr, index, x); // for multiple occurances of the same value using ArrayList
+        System.out.println(linearSearchFromLast(arr, arr.length - 1, 56)); // from (n - 1)th index
+        linearSearchMultipleOccurances(arr, 0, 0); // for multiple occurances of the same value using ArrayList
+        System.out.println(list);
+        linearSearchArrayList(arr, 0, 0, new ArrayList<>()); // passing ArrayList as an argument
+        System.out.println(list);
     }
 
     public static int linearSearch(int[] arr, int index, int x){
@@ -20,6 +24,7 @@ public class LinearSearch {
             return(linearSearch(arr, index + 1, x));
         }
     }
+
 
     public static int linearSearchFromLast(int[] arr, int index, int x){
         // base case
@@ -36,16 +41,29 @@ public class LinearSearch {
 
 
     public static ArrayList<Integer> list = new ArrayList<>();
-    public static ArrayList linearSearchMultipleOccurances(int[] arr, int index, int x){
+    public static void linearSearchMultipleOccurances(int[] arr, int index, int x){
         // base case
         if(index == arr.length){
-            return -1;
+            return;
         }
 
         if(arr[index] == x){
             list.add(index);
-        } else{
-            return(linearSearchMultipleOccurances(arr, index + 1, x));
+        } 
+        linearSearchMultipleOccurances(arr, index + 1, x);
+    }
+
+    
+    public static ArrayList<Integer> linearSearchArrayList(int[] arr, int index, int x, ArrayList<Integer> list){
+        // passing ArrayList as an argument in the recursive function
+        if(index == arr.length){
+            return list;
         }
+
+        if(arr[index] == x){
+            list.add(index);
+        }
+
+        return(linearSearchArrayList(arr, index + 1, x, list));
     }
 }
