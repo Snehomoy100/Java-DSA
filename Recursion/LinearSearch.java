39c86771c738c64e42f233c9a53d5c4d005211b7
snehomoy100@gmail.com <snehomoy100@gmail.com>
2021-11-29 19:08:27 +0530

    LinearSearch.java file added & updated with multiple occurances solution

--- Recursion/LinearSearch.java
+++ Recursion/LinearSearch.java
@@ -3,6 +3,7 @@ public class LinearSearch {
         int[] arr = {12, 3, 56, 0, 6, 0};
         System.out.println(linearSearch(arr, 0, 0)); // from 0th index
         System.out.println(linearSearchFromLast(arr, arr.length, 56)); // from (n - 1)th index
+        linearSearchMultipleOccurances(arr, index, x); // for multiple occurances of the same value
     }
 
     public static int linearSearch(int[] arr, int index, int x){
@@ -28,6 +29,19 @@ public class LinearSearch {
             return index;
         } else{
             return(linearSearchFromLast(arr, index - 1, x));
+        }
+    }
+
+    public static int linearSearchMultipleOccurances(int[] arr, int index, int x){
+        // base case
+        if(index == arr.length){
+            return -1;
+        }
+
+        if(arr[index] == x){
+            return index;
+        } else{
+            return(linearSearchMultipleOccurances(arr, index + 1, x));
         }
     }
 }
