2632e31b892005419ccbd0b03f63024caf15804b
snehomoy100@gmail.com <snehomoy100@gmail.com>
2021-12-02 11:15:36 +0530

    BubbleSort.java file added & updated with the function call

--- Recursion/BubbleSort.java
+++ Recursion/BubbleSort.java
@@ -1,9 +1,10 @@
 public class BubbleSort {
     public static void main(String[] args) {
-        
+        int[] arr = {4, 3, 2, 1, 0};
+        bubbleSortRecursion(arr, (arr.length - 1));
     }
 
-    public static void bubbleSortRecursion(int[] arr){
+    public static void bubbleSortRecursion(int[] arr, int lastIndex){
         
     }
 }
