61cb47065764a512ca05e48fa2754a0771963458
snehomoy100@gmail.com <snehomoy100@gmail.com>
2021-12-06 19:23:06 +0530

    BubbleSort.java file updated with the recursive bubble sort algorithm

--- Recursion/BubbleSort.java
+++ Recursion/BubbleSort.java
@@ -1,15 +1,31 @@
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {4, 3, 2, 1, 0};
-        bubbleSortRecursion(arr, (arr.length - 1));
+        bubbleSortRecursion(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static void bubbleSortRecursion(int[] arr, int lastIndex){
+    public static void bubbleSortRecursion(int[] arr, int size){
         
         // base case
-        if(lastIndex == 0) return;
+        if(size == 0) {
+            return;
+        }
         
-        if()
+        // iteraing over the array
+        for(int i=0; i<(size - 1); i++){
+
+            if(arr[i] > arr[i+1]){
+            // swap
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+            }
+        }
+
+        bubbleSortRecursion(arr, size - 1);
 
     }
 }
