f6c4c525e1f3835a970679dfc45c90b9ad181dac
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-01-24 21:53:32 +0530

    SkipString.java file updated with SkipSpecialString class

--- Recursion/SkipString.java
+++ Recursion/SkipString.java
@@ -1,7 +1,7 @@
-public class SkipString {
+class SkipString {
     public static void main(String[] args) {
-        String str = "helloiamhola";
-        String target = "hola";
+        String str = "helloiamsnehomoy";
+        String target = "hello";
         String ans = skipString(str, target);
         System.out.println(ans);
     }
@@ -9,15 +9,33 @@ public class SkipString {
     public static String skipString(String str, String target){
         // base case
         if(str.isEmpty()){ // str.length() = 0
+            return ""; // returns an empty string
+        }
+
+        if(str.startsWith(target)){
+            return skipString(str.substring(target.length()), target);
+        } else {
+            return (str.charAt(0) + skipString(str.substring(1), target));
+        }
+    }
+}
+
+
+class SkipSpecialString {
+    public static void main(String[] args) {
+        System.out.println(skipSpecialString("helloiamsnehomoy", "sne", "snehomoy"));
+    }
+
+    public static String skipSpecialString(String str, String target, String subTarget){
+        // base case 
+        if(str.isEmpty()){
             return "";
         }
 
-        String ch = str.substring(1);
-
-        if(ch.equals(target)){
-            return skipString(str.substring(1), target);
-        } else {
-            return ch + skipString(str, target);
+        if(str.startsWith(subTarget) && str.startsWith(target)){
+            return skipSpecialString(str.substring(subTarget.length()), target, subTarget);
+        } else{
+            return (str.charAt(0) + skipSpecialString(str.substring(1), target, subTarget));
         }
     }
 }
