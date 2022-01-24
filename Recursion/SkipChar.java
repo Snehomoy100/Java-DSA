3a66e340bcf93440387df7a3f1565df778890d21
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-01-24 20:24:13 +0530

    SkipChar.java file created & updated with the algo

---
+++ Recursion/SkipChar.java
@@ -0,0 +1,24 @@
+public class SkipChar {
+    public static void main(String[] args) {
+        String str = "helloiamhela";
+        char target = 'l';
+        String ans = skipChar(str, target);
+        System.out.println(ans);
+
+    }
+
+    public static String skipChar(String str, char target){
+        if(str.length() == 0){
+            return "";
+        }
+
+        char ch = str.charAt(0);
+
+        if(ch == target){
+            return skipChar(str.substring(1), target);
+        } else {
+            return ch + skipChar(str.substring(1), target);
+        }
+
+    }
+}
