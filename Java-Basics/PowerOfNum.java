77b700ec7a85d5023c6bc227f79612df8959ff19
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-02-07 23:20:34 +0530

    PowerOfNum.java file added & updated

---
+++ Java-Basics/PowerOfNum.java
@@ -0,0 +1,16 @@
+public class PowerOfNum {
+
+    public static int power(int number, int power){
+        if(power == 0){
+            return 1;
+        }
+
+        int res = 1;
+        res = res * number;
+        return power(number, power-1);
+    }
+    public static void main(String[] args) {
+        int ans = power(2, 3);
+        System.out.println(ans);
+    }
+}
--- Recursion/Sample.java
+++ Recursion/Sample.java
@@ -1,21 +1,23 @@
-import java.util.Scanner;
+public class Sample{
+        public static int lastOccurance(int arr[], int x, int n){
+        int low = 0, high = n - 1,
+        res = -1;
+        while (low <= high){
+        int mid = (low + high) / 2;
+        if (arr[mid] > x)
+        high = mid - 1;
+        else if (arr[mid] < x)
+        low = mid + 1;
+        else{
+        res = mid;
+        low = mid + 1;
+        }
+        }
+        return res;
+        }
 
-class Sample {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int A = input.nextInt();
-        int B = input.nextInt();
-        int addition = (A+B);
-        int substraction = (A-B);
-        int multiplication = (A*B);
-        int division = (A/B);
-        boolean isAddition = addition > substraction && addition > multiplication && addition > division;
-        boolean isSubstraction = substraction > addition && substraction > multiplication && substraction > division;
-        boolean isMultiplication = multiplication > addition && multiplication > substraction && multiplication > division;
-        boolean isDivision = division > addition && division > substraction && division > multiplication;
-        if(isAddition) System.out.println("+");
-        if(isSubstraction) System.out.println("-");
-        if(isMultiplication) System.out.println("*");
-        if(isDivision) System.out.println("/");
+        int[] arr = {1, 2, 2, 2, 2, 3, 4, 7, 8, 8};
+        System.out.println(lastOccurance(arr, 8, arr.length));
     }
 }
