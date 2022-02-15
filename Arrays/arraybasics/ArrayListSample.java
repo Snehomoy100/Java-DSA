15750ee806767d7c8235e2ac9d2f39bfb3fbf175
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-02-15 09:30:41 +0530

    ArrayListSample.java file added & updated with the ArrayList in-built methods

--- Arrays/arraybasics/ArrayListSample.java
+++ Arrays/arraybasics/ArrayListSample.java
@@ -1,9 +1,20 @@
 package arraybasics;
 
+import java.util.ArrayList;
+
 // example of ArrayList in Java
 
 public class ArrayListSample {
     public static void main(String[] args){
-        
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(20); // for adding any element in the ArrayList 
+        arr.add(30);
+        arr.add(40);
+        System.out.println(arr.size()); // for getting the size of the ArrayList
+        System.out.println(arr.get(1)); // for accessing a particular element using the index
+        arr.add(3, 34);
+        System.out.println(arr.size());
+        System.out.println(arr.get(0));
+        System.out.println(arr.get(3));
     }
 }
