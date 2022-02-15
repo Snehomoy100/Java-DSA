33f9ed2ba8d8d0f6917027aa1c2851a31c1e426d
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-02-15 09:23:58 +0530

    ArrayListSample.java file added & updated

--- Arrays/arraybasics/ArrayListSample.java
+++ Arrays/arraybasics/ArrayListSample.java
@@ -1,7 +1,9 @@
 package arraybasics;
 
+// example of ArrayList in Java
+
 public class ArrayListSample {
     public static void main(String[] args){
-        System.out.println("Hello..!");
+        
     }
 }
