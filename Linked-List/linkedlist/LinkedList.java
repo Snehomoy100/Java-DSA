32b7322648e5d6dfde57abfd2190e9f77beabf18
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-02-20 18:05:21 +0530

    Linkedlist node created & initialised with 10 as the value

--- Linked-List/linkedlist/LinkedList.java
+++ Linked-List/linkedlist/LinkedList.java
@@ -1,10 +1,24 @@
 package linkedlist;
 
 public class LinkedList {
+
+    public static Node<Integer> createLinkedlist(){
+        Node<Integer> n1 = new Node<>(10);
+        Node<Integer> n2 = new Node<>(20);
+        Node<Integer> n3 = new Node<>(30);
+        Node<Integer> n4 = new Node<>(40);
+
+        // creation of the linkedlist
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = null;
+
+        return n1;
+
+    }
     public static void main(String[] args) {
-        Node n1 = new Node(10);
-        System.out.println(n1); // the memory address of the node
-        System.out.println(n1.data); // the data element of the node
-        System.out.println(n1.next); // the memory address of the next node 
+        Node<Integer> head = createLinkedlist();
+        System.out.println(head.next);
     }
 }
--- Linked-List/linkedlist/Node.java
+++ Linked-List/linkedlist/Node.java
@@ -1,9 +1,9 @@
 package linkedlist;
 
-public class Node {
-    int data; // the data is being stored inside the linkedlist
-    Node next; // this is an empty object which has null as the value
-    Node(int data){
+public class Node<T> { // <> are the generics
+    T data; // the data is being stored inside the linkedlist
+    Node<T> next; // this is an empty object which has null as the value
+    Node(T data){
         this.data = data;
     }
 }
