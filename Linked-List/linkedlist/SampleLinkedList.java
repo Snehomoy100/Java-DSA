34d22bcbae28a3f19f23603c3ad65456c4730c86
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-02-20 21:15:06 +0530

    SampleLinedList.java file added & updated with the sample question

---
+++ Linked-List/linkedlist/SampleLinkedList.java
@@ -0,0 +1,20 @@
+package linkedlist;
+
+public class SampleLinkedList {
+
+    public static void print(Node<Integer> head){
+        Node<Integer> temp = head;
+
+        while(temp != null){
+            System.out.println(temp.data + "");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Node<Integer> node1 = new Node<>(10);
+        Node<Integer> node2 = new Node<>(20);
+        node2.next = node1;
+        print(node2); // prints 20 10
+    }
+}
