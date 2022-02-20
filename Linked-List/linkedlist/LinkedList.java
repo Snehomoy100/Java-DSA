3125778b8d7e029cedd907d9c9248d9bbd99d774
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-02-20 17:58:55 +0530

    Linkedlist node created & initialised with 10 as the value

--- Linked-List/linkedlist/LinkedList.java
+++ Linked-List/linkedlist/LinkedList.java
@@ -2,6 +2,9 @@ package linkedlist;
 
 public class LinkedList {
     public static void main(String[] args) {
-        System.out.println("LinkedList");
+        Node n1 = new Node(10);
+        System.out.println(n1); // the memory address of the node
+        System.out.println(n1.data); // the data element of the node
+        System.out.println(n1.next); // the memory address of the next node 
     }
 }
---
+++ Linked-List/linkedlist/Node.java
@@ -0,0 +1,9 @@
+package linkedlist;
+
+public class Node {
+    int data; // the data is being stored inside the linkedlist
+    Node next; // this is an empty object which has null as the value
+    Node(int data){
+        this.data = data;
+    }
+}
