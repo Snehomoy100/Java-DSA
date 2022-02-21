b226fe81798bff8fa1c5618eda1ee50b786761dd
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-02-21 20:09:42 +0530

    increment method added in the SampleLinkedList.java file & updated

--- Linked-List/linkedlist/SampleLinkedList.java
+++ Linked-List/linkedlist/SampleLinkedList.java
@@ -11,6 +11,16 @@ public class SampleLinkedList {
         }
         System.out.println();
     }
+
+
+    public static void increment(Node<Integer> head){
+        Node<Integer> temp = head;
+        while (temp != null) {
+            temp.data++;
+            temp = temp.next;
+        }
+    }
+    
     public static void main(String[] args) {
         Node<Integer> node1 = new Node<>(10);
         Node<Integer> node2 = new Node<>(20);
