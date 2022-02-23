f34dfddc1d4d40ee15128abda1b36e8008c597f9
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-02-23 20:02:59 +0530

    LengthOfLinkedList.java file updated with the linkedListLength method

--- Linked-List/linkedlist/LengthOfLinkedList.java
+++ Linked-List/linkedlist/LengthOfLinkedList.java
@@ -3,17 +3,24 @@ package linkedlist;
 public class LengthOfLinkedList {
 
     public static Node<Integer> createLinkedlist(){
+        // linked list creation phase
         Node<Integer> n1 = new Node<Integer>(10);
         Node<Integer> n2 = new Node<Integer>(20);
         Node<Integer> n3 = new Node<Integer>(30);
         Node<Integer> n4 = new Node<Integer>(40);
+
+        // linking phase
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4 = null;
 
         return n1;
     }
 
     public static int linkedListLength(Node<Integer> head){
         Node<Integer> temp = head;
-        int length = 0;
+        int length = -1; // as the Node no of first element should be 0
         while(temp != null){
             temp = temp.next;
             length++;
