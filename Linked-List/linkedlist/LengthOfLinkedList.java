6d0043ebf5f819073d56563cca70469b90557316
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-02-23 19:58:12 +0530

    LengthOfLinkedList.java file updated with the linkedListLength method

--- Linked-List/linkedlist/LengthOfLinkedList.java
+++ Linked-List/linkedlist/LengthOfLinkedList.java
@@ -1,7 +1,27 @@
 package linkedlist;
 
 public class LengthOfLinkedList {
+
+    public static Node<Integer> createLinkedlist(){
+        Node<Integer> n1 = new Node<Integer>(10);
+        Node<Integer> n2 = new Node<Integer>(20);
+        Node<Integer> n3 = new Node<Integer>(30);
+        Node<Integer> n4 = new Node<Integer>(40);
+
+        return n1;
+    }
+
+    public static int linkedListLength(Node<Integer> head){
+        Node<Integer> temp = head;
+        int length = 0;
+        while(temp != null){
+            temp = temp.next;
+            length++;
+        }
+        return length;
+    }
     public static void main(String[] args) {
-        
+        Node<Integer> head = createLinkedlist();
+        linkedListLength(head);
     }
 }
