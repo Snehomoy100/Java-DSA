ba09d41ba2b4ef4e1e86788bc40de00499780b45
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-03-17 22:31:19 +0530

    NodeInputLinkedList.java file added & updated

--- Linked-List/linkedlist/NodeLinkedList.java
+++ Linked-List/linkedlist/NodeLinkedList.java
@@ -1,8 +1,26 @@
 package linkedlist;
 
+import java.util.Scanner;
+
 public class NodeLinkedList {
 
     // insert a node in a given linkedlist
+
+    public static Node<Integer> insertNode(Node<Integer> head, int targetIndex, int data){
+        Node<Integer> temp = head;
+        int indexCount = 0;
+        while(temp != null){
+            if(indexCount == (targetIndex - 1)){
+                Node<Integer> currentNode = new Node<Integer>(data), nextNode = null;
+                nextNode = temp.next;
+                temp.next = currentNode;
+                currentNode.next = nextNode;
+            }
+            indexCount++;
+            temp = temp.next;
+        }
+        return head;
+    }
     public static void main(String[] args){
         
     }
