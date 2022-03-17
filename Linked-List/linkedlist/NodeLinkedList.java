5f24da17d3294015ba3df2b3171c519dcba67a70
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-03-17 21:55:03 +0530

    NodeInputLinkedList.java file added & updated

--- Linked-List/linkedlist/NodeLinkedList.java
+++ Linked-List/linkedlist/NodeLinkedList.java
@@ -2,7 +2,7 @@ package linkedlist;
 
 public class NodeLinkedList {
 
-    // insert a node in a linkedlist
+    // insert a node in a given linkedlist
     public static void main(String[] args){
         
     }
