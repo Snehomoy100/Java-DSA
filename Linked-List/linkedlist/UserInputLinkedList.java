0e295257884d01012179c99038ae52587bafa576
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-03-17 18:36:38 +0530

    UserInputLinkedList.java file added & updated

--- Linked-List/linkedlist/UserInputLinkedList.java
+++ Linked-List/linkedlist/UserInputLinkedList.java
@@ -2,10 +2,9 @@ package linkedlist;
 
 import java.util.Scanner;
 
-    // O(n^2) approach
-    
-public class UserInputLinkedList {
+class UserInputLinkedListBruteForce {
 
+    // O(n^2) approach
     // taking user input as the elements of Linked List
 
     public static void printLinkedlist(Node<Integer> head) {
@@ -33,6 +32,48 @@ public class UserInputLinkedList {
                 tail.next = currentNode; 
             }
             data = input.nextInt();           
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node<Integer> head = takeInput();
+        printLinkedlist(head);
+    }
+}
+
+
+
+
+class UserInputLinkedListOptimal {
+
+    // O(n) approach
+
+    public static void printLinkedlist(Node<Integer> head){
+        Node<Integer> temp = head;
+        while(temp != null){
+            System.out.println(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static Node<Integer> takeInput(){
+        Scanner input = new Scanner(System.in);
+        int data = input.nextInt();
+        Node<Integer> head = null;
+        Node<Integer> tail = null;
+
+        while(data != -1){
+            Node<Integer> currentHead = new Node<Integer>(data);
+            if(head == null){
+                head = currentHead;
+                tail = currentHead;
+            } else {
+                tail.next = currentHead;
+                tail = tail.next;
+            }
+            data = input.nextInt();
         }
         return head;
     }
