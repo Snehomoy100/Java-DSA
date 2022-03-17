001e3952e625b5c5f29368aed46dc908b1d43ae2
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-03-17 17:24:00 +0530

    UserInputLinkedList.java file added & updated

--- Linked-List/linkedlist/UserInputLinkedList.java
+++ Linked-List/linkedlist/UserInputLinkedList.java
@@ -6,37 +6,19 @@ public class UserInputLinkedList {
 
     // taking user input as the elements of Linked List
 
-    public static void printLinkedlist(Node<Integer> head){
+    public static void printLinkedlist(Node<Integer> head) {
         Node<Integer> temp = head;
-        while(temp != null){
+        while (temp != null) {
             System.out.print(temp.data + " ");
             temp = temp.next;
         }
         System.out.println();
     }
 
-
     public static Node<Integer> takeInput(){
-        Scanner input = new Scanner(System.in);
-        int data = input.nextInt();
-        Node<Integer> head = null;
-
-        while(data != -1){
-            Node<Integer> currentHead = new Node<Integer>(data);
-            if(head == null){
-                // make this as first node of the linkedlist
-                head = currentHead;
-            } else{
-                Node<Integer> tail = head;
-                while(tail.next != null){
-                    tail = tail.next;
-                }
-                tail.next = currentHead;
-            }
-            data = input.nextInt();
-        }
-        return head;
+        Scanner input 
     }
+
     public static void main(String[] args) {
         Node<Integer> head = takeInput();
         printLinkedlist(head);
