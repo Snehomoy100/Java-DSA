840bbba8583228c43f2cbb94becc83ba92b1a58b
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-03-17 22:33:57 +0530

    NodeInputLinkedList.java file added & updated

--- Linked-List/linkedlist/NodeLinkedList.java
+++ Linked-List/linkedlist/NodeLinkedList.java
@@ -21,6 +21,25 @@ public class NodeLinkedList {
         }
         return head;
     }
+
+    public static Node<Integer> takeInput(){
+        Scanner input = new Scanner(System.in);
+        int data = input.nextInt();
+        Node<Integer> head = null, tail = null;
+        while(data != -1){
+            Node<Integer> currentHead = new Node<Integer>(data);
+            if(head == null){
+                head = currentHead;
+                tail = currentHead;
+            } else {
+                tail.next = currentHead;
+                tail = tail.next;
+            }
+            data = input.nextInt();
+        }
+        return head;
+    }
+    
     public static void main(String[] args){
         
     }
