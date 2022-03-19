0c47bfe32ae9bd4b73e3e951c608603d4e432afc
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-03-19 20:51:39 +0530

    FindNode.java file updated with the takeInput method

--- Linked-List/linkedlist/FindNode.java
+++ Linked-List/linkedlist/FindNode.java
@@ -1,5 +1,7 @@
 package linkedlist;
 
+import java.util.Scanner;
+
 public class FindNode {
 
     public static int returnIndex(Node<Integer> head, int value){
@@ -13,6 +15,29 @@ public class FindNode {
             temp = temp.next;
         }
         return -1;
+    }
+
+    public static Node<Integer> takeInput(){
+        Scanner input = new Scanner(System.in);
+        int data = input.nextInt();
+        Node<Integer> head = null, tail = null;
+        while(data != -1){
+            Node<Integer> currentHead = new Node<Integer>(data);
+            if(head == null){
+                head = currentHead;
+                tail = currentHead;
+            } else {
+                tail = currentHead;
+                tail = tail.next;
+            }
+            data = input.nextInt();
+        }
+        return head;
+    }
+
+    public static void printLL(Node<Integer> head){
+        Node<Integer> temp = head;
+        
     }
     public static void main(String[] args) {
         
