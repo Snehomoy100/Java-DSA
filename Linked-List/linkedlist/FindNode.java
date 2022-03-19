3ee9287ed2978e1325bf0522adbae7710b86091b
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-03-19 20:26:02 +0530

    FindNode.java file added & updated with the returnIndex method

--- Linked-List/linkedlist/FindNode.java
+++ Linked-List/linkedlist/FindNode.java
@@ -1,6 +1,19 @@
 package linkedlist;
 
 public class FindNode {
+
+    public static int returnIndex(Node<Integer> head, int value){
+        Node<Integer> temp = head;
+        int index = 0;
+        while(temp != null){
+            if(temp.data == value){
+                return index;
+            }
+            index++;
+            temp = temp.next;
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         
     }
