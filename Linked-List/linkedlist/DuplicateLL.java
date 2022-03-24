74cfec0461b6da5aa26fc0b76b61c1ef2ab7ef5c
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-03-24 23:19:47 +0530

    DuplicateLL.java file updated woth printNode() method

--- Linked-List/linkedlist/DuplicateLL.java
+++ Linked-List/linkedlist/DuplicateLL.java
@@ -1,40 +1,6 @@
 package linkedlist;
 
 public class DuplicateLL {
-
-    public static Node<Integer> takeInput(){
-        Scanner input = new Scanner(System.in);
-        int data = input.nextInt();
-        Node<Integer> head = null, tail = null;
-        while(data != -1){
-            Node<Integer> currentHead = new Node<Integer>(data);
-            if(head == null){
-                head = currentHead;
-                tail = currentHead;
-            } else {
-                tail.next = currentHead;
-                tail = tail.next;
-            }
-            data = input.nextInt();
-        }
-        return head;
-    }
-
-    public static void printNode(Node<Integer> head, int index){
-        Node<Integer> temp = head;
-        int count = -1;
-
-        while(temp != null){
-            count++;
-            if(count == index){
-                System.out.println(temp.data);
-            }
-            temp = temp.next;
-        }
-        if(index > count){
-            System.out.println("The index is out of range...!");
-        }
-    }
     public static void main(String[] args) {
         
     }
