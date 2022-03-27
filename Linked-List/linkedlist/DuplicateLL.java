7edb9ca94dccbaf3bfc3f1749ef5c8ffd53612c4
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-03-27 12:51:48 +0530

    DuplicateLL.java file updated with deleteDuplicate method

--- Linked-List/linkedlist/DuplicateLL.java
+++ Linked-List/linkedlist/DuplicateLL.java
@@ -1,6 +1,22 @@
 package linkedlist;
 
+import java.util.*;
+
 public class DuplicateLL {
+
+    public static Node<Integer> deleteDuplicate(Node<Integer> head){
+        Node<Integer> currentNode = head;
+        while(currentNode!=null){
+            Node<Integer> nextNode = currentNode.next;
+            if(nextNode.data != null && currentNode.data != nextNode.data){
+                currentNode = nextNode;
+            } else {
+                currentNode.next = nextNode.next;
+            }
+        }
+        return currentNode;
+
+    } 
     public static void main(String[] args) {
         
     }
