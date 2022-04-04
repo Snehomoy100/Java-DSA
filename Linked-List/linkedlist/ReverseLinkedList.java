0d9c46492bf97bce6fbf310657e035155b18b475
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-04-04 21:50:39 +0530

    RecursiveLinkedList.java file updated reverseLL method

--- Linked-List/linkedlist/ReverseLinkedList.java
+++ Linked-List/linkedlist/ReverseLinkedList.java
@@ -1,7 +1,38 @@
 package linkedlist;
 
+import java.util.*;
+
 public class ReverseLinkedList {
+
+    public static void reverseLL(Node<Integer> head){
+        if(head == null){
+            return;
+        }
+
+        reverseLL(head.next);
+        System.out.print(head.data + " ");
+    }
+
+    public static Node<Integer> userInputLL(){
+        Scanner input = new Scanner(System.in);
+        int data = input.nextInt();
+        Node<Integer> head = null, tail = null;
+        while(data!=-1){
+            Node<Integer> currentHead = new Node<Integer>(data);
+            if(head == null){
+                head = currentHead;
+                tail = currentHead;
+            } else {
+                tail.next = currentHead;
+                tail = tail.next;
+            }
+            data = input.nextInt();
+        }
+        return head;
+
+    }
     public static void main(String[] args){
-        
+        Node<Integer> head = userInputLL();
+        reverseLL(head);
     }
 }
