3fd35f1658ce573e1f598eb127284729230b9f0c
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-04-10 14:15:58 +0530

    RecursiveLinkedList.java file added & updated

--- Linked-List/linkedlist/RecursiveLinkedList.java
+++ Linked-List/linkedlist/RecursiveLinkedList.java
@@ -1,11 +1,40 @@
 package linkedlist;
 
+import java.util.*;
+
+// printing the linkedlist in recursive format
+
 public class RecursiveLinkedList {
 
+    public static Node<Integer> takeInputLL(){
+        Scanner input = new Scanner(System.in);
+        int data = input.nextInt();
+        Node<Integer> head = null, tail = null;
+        while(data != -1){
+            Node<Integer> currentNode = new Node<Integer>(data);
+            if(head == null){
+                head = currentNode;
+                tail = currentNode;
+            } else {
+                tail.next = currentNode;
+                tail = tail.next;
+            }
+            data = input.nextInt();
+        }
+        return head;
+    }
+
     public static void printLLRecursive(Node<Integer> head){
-        
+        // base case
+        if(head == null){
+            return;
+        }
+
+        System.out.print(head.data + " ");
+        printLLRecursive(head.next); // recursive call
     }
     public static void main(String[] args) {
-        
+        Node<Integer> head = takeInputLL();
+        printLLRecursive(head);
     }
 }
