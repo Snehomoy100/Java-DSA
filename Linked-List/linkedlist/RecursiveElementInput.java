8fcc1a623d9786752db8c82c4ff760dab76352f9
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-04-14 14:45:29 +0530

    updated

--- Linked-List/linkedlist/RecursiveElementInput.java
+++ Linked-List/linkedlist/RecursiveElementInput.java
@@ -4,7 +4,9 @@ package linkedlist;
 
 public class RecursiveElementInput {
 
-    public static Node<Integer> elementInput(Node<Integer> head, int targetIndex){
+    public static Node<Integer> elementInput(Node<Integer> head, int targetIndex, int element){
+        
+        // base case 
         
     }
     public static void main(String[] args) {
