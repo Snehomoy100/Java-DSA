885bf8a4a356de5eff6dfb15be315cfe1d5859eb
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-04-23 23:12:51 +0530

    OopSample.java file added & updated

---
+++ OOPS/oops/OopsSample.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class OopsSample {
+    public static void main(String[] args) {
+        Student s1 = new Student(); // creation of a new object
+        s1.rollNo = 10; // properties of the class
+        s1.name = "Snehomoy"; // properties of the class
+        System.out.println(s1.name + " " + s1.rollNo);
+    }
+    
+}
---
+++ OOPS/oops/Student.java
@@ -0,0 +1,6 @@
+package oops;
+
+public class Student {
+    int rollNo;
+    String name;
+}
