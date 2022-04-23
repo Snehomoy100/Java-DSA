5b09a565b586da4b099ab2de1357cd3ab78a8b67
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-04-23 23:23:20 +0530

    OopsSample.java file updated

--- OOPS/oops/OopsSample.java
+++ OOPS/oops/OopsSample.java
@@ -1,7 +1,8 @@
-package oops;
+package oops; // this package stores the Student class inside of it 
 
 public class OopsSample {
     public static void main(String[] args) {
+        // student class be accessed over here because of the oops package
         Student s1 = new Student(); // creation of a new object
         s1.rollNo = 10; // properties of the class
         s1.name = "Snehomoy"; // properties of the class
