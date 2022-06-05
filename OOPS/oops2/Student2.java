aba241b68cc71e61a69a44571a29c1c4eb317c78
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-06-05 09:50:40 +0530

    Student2.java updated with the s1 object

--- OOPS/oops2/Student2.java
+++ OOPS/oops2/Student2.java
@@ -4,10 +4,10 @@ import oops.Student; // for importing functionalities of classes of diff package
 
 public class Student2 {
     public static void main(String[] args) {
-        Student s1 = new Student();
-        s1.courseName = "Computer Science";
+        Student s1 = new Student(); // instance of the class (object)
+        s1.courseName = "Computer Science"; // accessing the props of the class 
         System.out.println(s1.courseName);
-        int rollNo = s1.getRoll(10);
+        int rollNo = s1.getRoll(10); // getters 
         System.out.println(rollNo);
     }
 }
