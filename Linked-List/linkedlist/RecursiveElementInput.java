4c5846f1ad8d4d71878c39754b5687c9e973cf8a
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-06-18 09:05:39 +0530

    RecursiveElementInput.java file updated with the elementInput method

--- Linked-List/linkedlist/RecursiveElementInput.java
+++ Linked-List/linkedlist/RecursiveElementInput.java
@@ -1,16 +1,51 @@
 package linkedlist;
 
-// import java.util.*;
+import java.util.*;
 
 public class RecursiveElementInput {
 
-    public static Node<Integer> elementInput(Node<Integer> head, int targetIndex, int element){
-        
+    public static Node<Integer> takeInputLL(){
+        Scanner input = new Scanner(System.in);
+        int data = input.nextInt();
+        Node<Integer> head = null, tail = null;
+        while(data != -1){
+            Node<Integer> currentNode = new Node<Integer>(data);
+            if(head == null){
+                head = currentNode;
+                tail = currentNode;
+            } else {
+                tail.next = currentNode;
+                tail = tail.next;
+            }
+            data = input.nextInt();
+        }
+        return head;
+    }
+
+    public static void elementInput(Node<Integer> head, int currentIndex, int element){
         // base case 
-        
-        // recurrence relation
+        if(head == null){
+            return;
+        }
+        // logic part 
+        if(currentIndex == 0){
+            Node<Integer> current = new Node<Integer>(element);
+            head = current;
+        }
+        // recursive func call
+        elementInput(head.next, currentIndex+1, element);
+    }
+
+    public static void printLL(Node<Integer> head){
+        Node<Integer> temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " ");
+        }
+        System.out.println();
     }
     public static void main(String[] args) {
-        
+        Node<Integer> head = takeInputLL();
+        elementInput(head, 0, 20);
+        printLL(head);
     }
 }
