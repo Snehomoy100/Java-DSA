8b050f0c402b13ba8703c672713b531c2b284b7e
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-06-21 11:19:59 +0530

    DeleteRecursive.java file added & updated

---
+++ Linked-List/linkedlist/DeleteRecursive.java
@@ -0,0 +1,14 @@
+package linkedlist;
+
+import java.util.*;
+
+public class DeleteRecursive {
+
+    public static Node<Integer> delRecursive(Node<Integer> head, int position){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
